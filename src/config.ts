581202235b2884f9ddafa151deb4ab17384f498a
Cinzya <cynti-ebert@web.de>
2024-02-13 12:27:42 +0100

    chore: add configuration instructions in configs

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,26 @@ export interface Config {
 export const getConfigFields = (): SomeCompanionConfigField[] => {
   return [
     {
+      type: 'static-text',
+      id: 'info',
+      width: 12,
+      label: 'Getting Started',
+      value: `This module utilizes the League of Legends <a href="https://developer.riotgames.com/docs/lol#game-client-api_replay-api">Replay API</a>, to adjust the in-game camera during replays.
+      <br></br>
+      By default the Replay API is disabled. To start using this module, enable the Replay API in the game client config by locating where your game is installed and adding the following lines to the <em>game.cfg</em> file:
+      <br></br>
+      Example file location:
+      </br>
+      <em>C:\\Riot Games\\League of Legends\\Config\\game.cfg</em>
+      <br></br>
+      <code>[General]</br>
+      EnableReplayApi=1
+      </code>
+      <br></br>
+      For more information on the Replay API, visit the <a href="https://developer.riotgames.com/docs/lol#game-client-api_replay-api">Riot Games Developer Portal</a>.
+      `,
+    },
+    {
       type: 'textinput',
       id: 'host',
       label: 'Taget host',
